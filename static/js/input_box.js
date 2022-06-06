98d90fef24a3021321bbd3e456018b8bbbbc142d
Griga178 <grisha.tis@gmail.com>
2022-06-06 22:03:44 +0300

    - - -

--- static/js/input_box.js
+++ static/js/input_box.js
@@ -1,8 +1,8 @@
-function create_input(){
+function create_input() {
   let label_inp = document.createElement("label");
   label_inp.setAttribute("for", "box_inp")
   label_inp.innerHTML = "Текст для input:"
-  
+
   let box_inp = document.createElement("input");
   box_inp.setAttribute("type", "text")
   box_inp.setAttribute("placeholder", "inp_box")
@@ -10,4 +10,10 @@ function create_input(){
 
   form.appendChild(label_inp)
   form.appendChild(box_inp)
+}
+
+function create_form_block(block_id) {
+  let block_form = document.createElement("div")
+  block_form.setAttribute('class', "setting_block")
+  form.appendChild(block_form)
 }
